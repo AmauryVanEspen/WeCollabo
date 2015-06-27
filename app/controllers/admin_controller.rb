b42113bbeecea35f114cd95dbc632f1de6431279
CarolineGao <curiousgao@gmail.com>
2015-06-27 14:20:42 +1000

    initial commit

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+
+  # load_and_authorize_resource - not a soultion to solve the authorizention issue.
+  #before_action :authenticate_user!
+  def index
+  end
+end
---
+++ app/views/profiles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @profile, :id, :user_id, :phone, :name, :profile_pic, :bio, :created_at, :updated_at
